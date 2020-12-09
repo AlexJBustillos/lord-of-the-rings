b4be6f303d985858a5458ea1c42b15bbd66fe88b
alexjbustillos <alexjbustillos@gmail.com>
2020-12-09 09:42:49 -0500

    [map]using map to unpack movieData

--- src/App.js
+++ src/App.js
@@ -5,6 +5,31 @@ import Movie from './Movie';
 
 class App extends Component {
   render () {
+    const movieData = [
+      {
+        title: "The Fellowship of the Ring",
+        hours: "2",
+        minutes: "58"
+      }, 
+      {
+        title: "The Two Towers",
+        hours: "2",
+        minutes: "59"
+
+      },
+      {
+        title: "The Return of the King",
+        hours: "3",
+        minutes: "21"
+
+      }
+    ]
+
+    const movieComponents = movieData.map((d, index) => {
+      // return <Movie title={d.title} hours={d.hours} minutes={d.minutes} key={'movie-' + index}/>
+      return <Movie { ...d } key={'movie-' + index}/>
+    })
+
     return (
       <div className="App">
         <header className="App-header">
@@ -12,9 +37,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <Movie title="The Fellowship of the Ring" hours="2" minutes="58" />
-          <Movie title="The Two Towers" hours="2" minutes="59" />
-          <Movie title="The Return of the King" hours="3" minutes="21" />
+            {movieComponents}
           <a
             className="App-link"
             href="https://reactjs.org"
